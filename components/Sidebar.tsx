a1f5c61c741e08669b8ed808735305de31a961cd
Balaganesh003 <k.balaganesh26@gmail.com>
2023-01-09 21:24:01 +0530

    added google and twitter sign-in using next-auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   BellIcon,
   HashtagIcon,
@@ -8,10 +8,12 @@ import {
   MailIcon,
   UserIcon,
   HomeIcon,
-} from '@heroicons/react/outline';
-import SidebarRow from './SidebarRow';
+} from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
+import { signOut, useSession, signIn } from 'next-auth/react'
 
 const Sidebar = () => {
+  const { data: session } = useSession()
   return (
     <div className="w-fit flex flex-col col-span-2 items-center px-4 md:items-start">
       <img className="m-3 w-10 h-10 " src="https://links.papareact.com/drq" />
@@ -21,10 +23,14 @@ const Sidebar = () => {
       <SidebarRow Icon={MailIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
       <SidebarRow Icon={CollectionIcon} title="Lists" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow
+        onClick={session ? signOut : signIn}
+        Icon={UserIcon}
+        title={session ? 'Sign Out' : 'Sign In'}
+      />
       <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,19 +1,22 @@
-import React, { SVGProps } from 'react';
+import React, { SVGProps } from 'react'
 
 interface Props {
-  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
-  title: string;
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
+  onClick?: () => {}
 }
 
-const SidebarRow = ({ Icon, title }: Props) => {
+const SidebarRow = ({ Icon, title, onClick }: Props) => {
   return (
-    <div className="flex gap-4  items-center px-4 py-3 cursor-pointer hover:shadow-md rounded-full transition-all duration-200 group active:scale-90">
+    <div
+      onClick={() => onClick && onClick()}
+      className="flex gap-4  items-center px-4 py-3 cursor-pointer hover:shadow-md rounded-full transition-all duration-200 group active:scale-90">
       <Icon className="w-8 h-8" />
       <p className="group-hover:text-twitter lg:text-xl font-semibold  text-base hidden md:inline ">
         {title}
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default SidebarRow;
+export default SidebarRow
